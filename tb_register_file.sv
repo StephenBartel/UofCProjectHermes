27a92560a84cc35757b3d116465fccffeafdbbcd
StephenBartel <stephen.bartel@ucalgary.ca>
2020-12-01 22:54:09 -0700

    Some fixes to Register File testbenc

--- tb_register_file.sv
+++ tb_register_file.sv
@@ -46,24 +46,40 @@ module tb_register_file;
     //**END CLOCK**//   
     
     //Instantiate the component we would like to test. 
-    register_file UUT(.clk(clk),.dst(dst_input),.src(src_input),.dstRead(dstRead_output),.srcRead(srcRead_output),.dstWrite(dstWrite_input),.writeEnable(writeEnable_input);
+    register_file UUT(.clk(clk),.dst(dst_input),.src(src_input),.dstRead(dstRead_output),.srcRead(srcRead_output),.dstWrite(dstWrite_input),.writeEnable(writeEnable_input));
     
-    //Initialize the input values
-    /*
-    initial begin
-            writeEnable_input = 1'b0; 
-            dst_input = 4'b0; 
-            src_input = 4'b0; 
-            dstWrite_input = 64'b0;
-    end
-    */
-    
-    writeEnable_input_vector = {1'b1,1'b0,1'b1,1'b0,1'b1,1'b0,1'b1,1'b0,1'b1,1'b0,1'b1,1'b0,1'b1,1'b0,1'b1,1'b0,1'b1,1'b0,1'b1,1'b0,1'b1,1'b0,1'b1,1'b0,1'b1,1'b0,1'b1,1'b0,1'b1,1'b0,1'b1,1'b0};
-    
-    dst_input_vector = {4'b0000,4'b0001,4'b0010,4'b0011,4'b0100,4'b0101,4'b0110,4'b0111,4'b1000,4'b1001,4'b1010,4'b1011,4'b1100,4'b1101,4'b1110,4'b1111};
-    src_input_vector = {4'b0000,4'b0001,4'b0010,4'b0011,4'b0100,4'b0101,4'b0110,4'b0111,4'b1000,4'b1001,4'b1010,4'b1011,4'b1100,4'b1101,4'b1110,4'b1111};
+    reg  [31:0] writeEnable_input_vector;
+    reg  [4:0] dst_input_vector [15:0];
+    reg  [4:0] src_input_vector [15:0]; 
+    reg  [63:0] dstWrite_input_vector [15:0];
+    reg  [63:0] expectedOutput; 
     
     
+    //Alternating 1 and 0 for the write enable. 
+    writeEnable_input_vector= {
+                               1'b1,1'b0,
+                               1'b1,1'b0,
+                               1'b1,1'b0,
+                               1'b1,1'b0,
+                               1'b1,1'b0,
+                               1'b1,1'b0,
+                               1'b1,1'b0,
+                               1'b1,1'b0,
+                               1'b1,1'b0,
+                               1'b1,1'b0,
+                               1'b1,1'b0,
+                               1'b1,1'b0,
+                               1'b1,1'b0,
+                               1'b1,1'b0,
+                               1'b1,1'b0,
+                               1'b1,1'b0
+                               };
+                               
+    //All combinations of the register select. 
+    dst_input_vector={4'b0000,4'b0001,4'b0010,4'b0011,4'b0100,4'b0101,4'b0110,4'b0111,4'b1000,4'b1001,4'b1010,4'b1011,4'b1100,4'b1101,4'b1110,4'b1111};
+    src_input_vector={4'b0000,4'b0001,4'b0010,4'b0011,4'b0100,4'b0101,4'b0110,4'b0111,4'b1000,4'b1001,4'b1010,4'b1011,4'b1100,4'b1101,4'b1110,4'b1111};
+    
+    //Randomized inputs that will be written and read to and from the register file as part of validating integrity.
     dstWrite_input_vector = {64'b0001110010001111101111001011111110110101010011010111000011110000,64'b1010111110101100001001110001010110100111011111100111000110010000,
                             64'b1100100010000001110001001000001010101001111011001000001011001110,64'b1101011110001001011111111010000100000100101010010110011001000001,
                             64'b1101001100001111110111111110001111000101101010110011101010110111,64'b0011101101110000011001101100001110000000101101011001010011001001,
@@ -72,13 +88,13 @@ module tb_register_file;
                             64'b0001001101000000011100101100111001101011010110000100011100110001,64'b1111000000101110110000111100100000011100000011110111011011100111,
                             64'b0010110101001001000100011011000010001010010100001100011011010010,64'b1110110110100010011000110111111110011001001100001001010100100000,
                             64'b0110001110100011010110001100100000011001110110000110111110010001,64'b0110001100000110100001000111111011111110010101111111100110101110};
-                            
-    
-    dstRead_output_expected = {}; 
-    srcRead_output_expected = {}; 
+             
+    //Counters that are used in conjunction with the above vectors in the below synchronous always block.                
     int writeCounter = 0; 
     int readCounter = 0; 
     int totalCounter = 0; 
+    
+    //Prinary testing block 
     
      //**SYNCHRONOUS ALWAYS BLOCK**//
     always @(posedge clk)
@@ -86,28 +102,32 @@ module tb_register_file;
             writeEnable_input = writeEnable_input_vector[totalCounter]; 
             
             //Write cycle
-            if(writeEnable_input == 1'b1) begin
+            if(writeEnable_input == 1'b1) 
+            begin
                 dst_input = dst_input_vector[writeCounter];
                 dstWrite_input=dstWrite_input_vector[writeCounter];
                 writeCounter+=1; 
             //Read cycle
-            else 
-                dst_input = dst_input_vector[readCounter];
-                src_input = src_input_vector[readCounter]; 
-                expectedOutput = dstWrite_input_vector[readCounter];
-                if(dstRead_output!=expectedOutput)begin
-                    $display("Test Failed for dstRead_output. dstRead=%d \t||\t expectedOutput=%d",dstRead_output,expectedOutput);
+            end else 
+                begin
+                    dst_input = dst_input_vector[readCounter];
+                    src_input = src_input_vector[readCounter]; 
+                    expectedOutput = dstWrite_input_vector[readCounter];
+                    if(dstRead_output!=expectedOutput)
+                    begin
+                        $display("Test Failed for dstRead_output. dstRead=%d \t||\t expectedOutput=%d",dstRead_output,expectedOutput);
+                    end
+                    if(srcRead_output!=expectedOutput)
+                    begin
+                        $display("Test Failed for srcRead_output. srcRead=%d \t||\t expectedOutput=%d",srcRead_output,expectedOutput);
+                    end
+                    readCounter+=1;
                 end
-                if(srcRead_output!=expectedOutput)begin
-                    $display("Test Failed for srcRead_output. srcRead=%d \t||\t expectedOutput=%d",srcRead_output,expectedOutput);
-                end
-                readCounter+=1;
-            end
             
             if(totalCounter>=31)
-                $finish
+                $finish; //Have 32 elements in writeEnable vector. Must stop here. 
+                
             totalCounter+=1;
-
         end
    //**SYNCHRONOUS ALWAYS BLOCK END**//
    
