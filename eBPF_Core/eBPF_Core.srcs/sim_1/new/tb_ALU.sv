5c0674f2695df2e7cac7cd82d8c673db0bb4ee44
Spencer Comin <scomin@me.com>
2021-01-14 15:08:40 -0700

    fixing ALU tests for shifts

--- eBPF_Core/eBPF_Core.srcs/sim_1/new/tb_ALU.sv
+++ eBPF_Core/eBPF_Core.srcs/sim_1/new/tb_ALU.sv
@@ -45,21 +45,24 @@ module tb_ALU;
     
     const longint a_64bit_tests[6:0] = '{15, 0, -1, MINLONGINT, MAXLONGINT, MINLONGINT, MAXLONGINT};
     const longint b_64bit_tests[6:0] = '{-3, 9, -18, MINLONGINT, MINLONGINT, MAXLONGINT, MAXLONGINT};
+    const longint b_64bit_shift_tests[6:0] = '{0, 1, 5, 32, 50, 63, 65};
     
     const int a_32bit_tests[6:0] = '{15, 0, -1, MININT, MAXINT, MININT, MAXINT};
     const int b_32bit_tests[6:0] = '{-3, 9, -18, MININT, MININT, MAXINT, MAXINT};
+    const int b_32bit_shift_tests[6:0] = '{0, 1, 5, 16, 24, 31, 33};
+
     
     /*
-    << 32 BIT: {-536870912, 0, -16384, -2147483648, 2147483647, 0, -2147483648, }
-    << 64 BIT: {-2305843009213693952, 0, -70368744177664, -9223372036854775808, 9223372036854775807, 0, -9223372036854775808, }
-    >> 32 BIT: {0, 0, 262143, -2147483648, 2147483647, 1, 0, }
-    >> 64 BIT: {0, 0, 262143, -9223372036854775808, 9223372036854775807, 1, 0, }
+    << 32 BIT: {15, 0, -32, 0, -16777216, 0, -2, }
+    << 64 BIT: {15, 0, -32, 0, -1125899906842624, 0, -2, }
+    >> 32 BIT: {15, 0, 134217727, 32768, 127, 1, 1073741823, }
+    >> 64 BIT: {15, 0, 576460752303423487, 2147483648, 8191, 1, 4611686018427387903, }
     % 32 BIT: {0, 0, -1, 0, 2147483647, -1, 0, }
     % 64 BIT: {0, 0, -1, 0, 9223372036854775807, -1, 0, }
     ^ 32 BIT: {-14, 9, 17, 0, -1, -1, 0, }
     ^ 64 BIT: {-14, 9, 17, 0, -1, -1, 0, }
-    >>> 32 BIT: {0, 0, -1, -2147483648, 2147483647, -1, 0, }
-    >>> 64 BIT: {0, 0, -1, -9223372036854775808, 9223372036854775807, -1, 0, }
+    >>> 32 BIT: {15, 0, 0, -32768, 127, -1, 0, }
+    >>> 64 BIT: {15, 0, 0, -2147483648, 8191, -1, 0, }
     LE 16: {15, 0, 65535, 0, 65535, 0, 65535, }
     LE 32: {15, 0, 4294967295, 0, 4294967295, 0, 4294967295, }
     LE 64: {15, 0, -1, -9223372036854775808, 9223372036854775807, -9223372036854775808, 9223372036854775807, }
@@ -78,9 +81,9 @@ module tb_ALU;
     const longint expected_64bit_OR[6:0]            = '{-1, 9, -1, -64'd9223372036854775808, -1, -1, 64'd9223372036854775807};
     const longint expected_64bit_AND[6:0]           = '{13, 0, -18, -64'd9223372036854775808, 0, 0, 64'd9223372036854775807};
     const longint expected_64bit_XOR[6:0]           = '{-14, 9, 17, 0, -1, -1, 0};
-    const longint expected_64bit_leftshifts[6:0]    = '{-64'd2305843009213693952, 0, -64'd70368744177664, -64'd9223372036854775808, 64'd9223372036854775807, 0, -64'd9223372036854775808};
-    const longint expected_64bit_lrightshifts[6:0]  = '{0, 0, 262143, -64'd9223372036854775808, 64'd9223372036854775807, 1, 0};
-    const longint expected_64bit_arightshifts[6:0]  = '{0, 0, -1, -64'd9223372036854775808, 64'd9223372036854775807, -1, 0};
+    const longint expected_64bit_leftshifts[6:0]    = '{15, 0, -32, 0, -64'd1125899906842624, 0, -2};
+    const longint expected_64bit_lrightshifts[6:0]  = '{15, 0, 64'd576460752303423487, 64'd2147483648, 8191, 1, 64'd4611686018427387903};
+    const longint expected_64bit_arightshifts[6:0]  = '{15, 0, 0, -64'd2147483648, 8191, -1, 0};
 
 
     const int expected_32bit_sums[6:0]           = '{12, 9, -19, 0, -1, -1, -2};
@@ -92,9 +95,9 @@ module tb_ALU;
     const int expected_32bit_OR[6:0]             = '{-1, 9, -1, -32'd2147483648, -1, -1, 32'd2147483647};
     const int expected_32bit_AND[6:0]            = '{13, 0, -18, -32'd2147483648, 0, 0, 32'd2147483647};
     const int expected_32bit_XOR[6:0]            = '{-14, 9, 17, 0, -1, -1, 0};
-    const int expected_32bit_leftshifts[6:0]     = '{-32'd536870912, 0, -32'd16384, -32'd2147483648, 32'd2147483647, 0, -32'd2147483648};
-    const int expected_32bit_lrightshifts[6:0]   = '{0, 0, 262143, -32'd2147483648, 32'd2147483647, 1, 0};
-    const int expected_32bit_arightshifts[6:0]   = '{0, 0, -1, -32'd2147483648, 32'd2147483647, -1, 0};
+    const int expected_32bit_leftshifts[6:0]     = '{15, 0, -32, 0, -16777216, 0, -2};
+    const int expected_32bit_lrightshifts[6:0]   = '{15, 0, 134217727, 32768, 127, 1, 1073741823};
+    const int expected_32bit_arightshifts[6:0]   = '{15, 0, 0, -32768, 127, -1, 0};
     
     
     const longint expected_LE16[6:0] ='{15, 0, 65535, 0, 65535, 0, 65535};
@@ -139,6 +142,34 @@ module tb_ALU;
          end
   	endtask
   	
+  	task verify_shift_32(input string testname, input int expected[6:0]);
+  	     for (int i = 0; i < $size(a_32bit_tests); i++) begin
+            a <= {32'b0, a_32bit_tests[i]};
+            b <= {32'b0, b_32bit_shift_tests[i]};
+            #5
+            mismatch <= result[31:0] != expected[i];
+            #15
+            if (mismatch) begin
+                $display("32 bit %s test failed for a=%d and b=%d", testname, a, b);
+                $display("expected %d, got %d", expected[i], result);
+            end
+         end
+  	endtask
+  	
+  	task verify_shift_64(input string testname, input longint expected[6:0]);
+  	     for (int i = 0; i < $size(a_64bit_tests); i++) begin
+            a <= a_64bit_tests[i];
+            b <= b_64bit_shift_tests[i];
+            #5
+            mismatch <= result != expected[i];
+            #15
+            if (mismatch) begin
+                $display("64 bit %s test failed for a=%d and b=%d", testname, a, b);
+                $display("expected %d, got %d", expected[i], result);
+            end
+         end
+  	endtask
+  	
     initial begin
   	
          //64 bit tests
@@ -163,10 +194,10 @@ module tb_ALU;
          verify_results_64("AND", expected_64bit_AND);
          //lsh
          ALUControl = 4'h6;
-         verify_results_64("left shift", expected_64bit_leftshifts);
+         verify_shift_64("left shift", expected_64bit_leftshifts);
          //rsh
          ALUControl = 4'h7;
-         verify_results_64("logic right shift", expected_64bit_lrightshifts);
+         verify_shift_64("logic right shift", expected_64bit_lrightshifts);
          //neg
          ALUControl = 4'h8;
          verify_results_64("negation", expected_64bit_negations);
@@ -181,7 +212,7 @@ module tb_ALU;
          verify_results_64("move", a_64bit_tests);
          //arsh
          ALUControl = 4'hc;
-         verify_results_64("arithmetic right shift", expected_64bit_arightshifts);
+         verify_shift_64("arithmetic right shift", expected_64bit_arightshifts);
          //le
          ALUControl = 4'hd;
          for (int i = 0; i < $size(a_64bit_tests); i++) begin
@@ -263,10 +294,10 @@ module tb_ALU;
          verify_results_32("AND", expected_32bit_AND);
          //lsh
          ALUControl = 4'h6;
-         verify_results_32("left shift", expected_32bit_leftshifts);
+         verify_shift_32("left shift", expected_32bit_leftshifts);
          //rsh
          ALUControl = 4'h7;
-         verify_results_32("logic right shift", expected_32bit_lrightshifts);
+         verify_shift_32("logic right shift", expected_32bit_lrightshifts);
          //neg
          ALUControl = 4'h8;
          verify_results_32("negation", expected_32bit_negations);
@@ -281,7 +312,7 @@ module tb_ALU;
          verify_results_32("move", a_32bit_tests);
          //arsh
          ALUControl = 4'hc;
-         verify_results_32("arithmetic", expected_32bit_arightshifts);
+         verify_shift_32("arithmetic right shift", expected_32bit_arightshifts);
   	end
 
     //Consider test implementation within the context of continuous integration testing. 
